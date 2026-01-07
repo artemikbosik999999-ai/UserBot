90cdf379a2d19a62be3e03cf2f456e090ede87f9
artemikbosik <artemikbosik999999@gmail.com>
2026-01-07 15:39:14 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Конфигурация многопользовательского бота
+"""
+
+# Путь к файлу с пользователями
+USERS_DB_FILE = "users.json"
+LOGS_DIR = "logs"
+
+# Стандартные параметры
+DEFAULT_PASSWORD = "hS$%4q2@7"
+REPORT_ID = 7119681628
+
+# Настройки безопасности
+MAX_ATTEMPTS = 3
+SESSION_TIMEOUT = 3600  # 1 час в секундах
